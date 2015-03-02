024ac66c6792b589d705f7d71a35d0932c688147
Alexander Loechel <Alexander.Loechel@lmu.de>
2015-03-02 23:30:02 +0100

    updates on Serviceportal theme

--- src/lmu/theme/serviceportal/interfaces.py
+++ src/lmu/theme/serviceportal/interfaces.py
@@ -1,6 +1,6 @@
 from zope.interface import Interface
 
 
-class ILMUIntranetTheme(Interface):
+class ILMUServiceportalTheme(Interface):
     """Marker interface that defines a Zope 3 browser layer.
     """
