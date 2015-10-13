7299c52902518c71555cffb06bcf655d454843ba
User for Plone <plone_buildout@cms-live-app3.verwaltung.uni-muenchen.de>
2015-10-13 10:11:03 +0200

    update extras.js so that it is empty

--- src/lmu/theme/serviceportal/js/extras.js
+++ src/lmu/theme/serviceportal/js/extras.js
@@ -1,11 +1,2 @@
 /* extras.js */
 
-$.ajax(cache: false)
-
-$("input#form-buttons-save").addClass("button small")
-$("input#form-buttons-cancel").addClass("button small")
-$("input#form-buttons-comment").addClass("button small")
-/*
-$("input#form-widgets-ILeadImage-image-input").addClass("button small")
-*/
-
